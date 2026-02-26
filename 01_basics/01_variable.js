2259b28ce9e24b2e299b52de55ee438c7719d5ae
Ayush12345-Gupta <ayushgupta5300@gmail.com>
2026-02-26 18:15:37 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const eccountID=124455
+let eccountEmil="yuh@google.com"
+var ccountPwor="123456"
+AcccountCity="jAipur"
+
+//eccountID=2// not AlloweD
+eccountEmil="Ayuh@52634"
+AcccountCity="chenni";
+console.log(eccountID);
+console.log(eccountEmil);
+console.log(AcccountCity);
+console.table([eccountID,eccountEmil,AcccountCity])
